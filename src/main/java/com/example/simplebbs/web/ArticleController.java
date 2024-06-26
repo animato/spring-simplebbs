39d296d26e191c27e66f5bbbf207745a38f306be
dsyang <dsyang.tech@gmail.com>
2024-06-26 16:53:33 +0900

    글 작성 기능 구현
    form으로 부터 데이터 전달

--- src/main/java/com/example/simplebbs/web/ArticleController.java
+++ src/main/java/com/example/simplebbs/web/ArticleController.java
@@ -1,13 +1,23 @@
 package com.example.simplebbs.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
 
     @GetMapping("/write")
-    public String writePage() {
+    public String writePage(Model model) {
+        model.addAttribute("articleInput", new ArticleInput());
         return "write";
+    }
+
+    @PostMapping("/write")
+    public String submitForm(@ModelAttribute ArticleInput articleInput) {
+        System.out.printf("Article submitted: %s %s %s %n", articleInput.subject, articleInput.contents, articleInput.author);
+        return "redirect:/write";
     }
 }
---
+++ src/main/java/com/example/simplebbs/web/ArticleInput.java
@@ -0,0 +1,32 @@
+package com.example.simplebbs.web;
+
+public class ArticleInput {
+    String subject;
+    String contents;
+    String author;
+
+    // Getter와 Setter
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
