25de3eebb37159ab32b2d125fa7d9bef794b9ed2
dsyang <dsyang.tech@gmail.com>
2024-07-01 15:14:04 +0900

    사용자 기능 구현
    로그인 구현

--- src/main/java/com/example/simplebbs/user/UserMapper.java
+++ src/main/java/com/example/simplebbs/user/UserMapper.java
@@ -2,10 +2,15 @@ package com.example.simplebbs.user;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     // 새로운 사용자 정보를 데이터베이스에 삽입하는 메소드
     @Insert("INSERT INTO user (username, password) VALUES (#{username}, #{password})")
     void insertUser(User user);
+
+    // 사용자 이름을 매개변수로 받아 해당 사용자의 정보를 불러오는 메소드
+    @Select("SELECT * FROM user WHERE username = #{username}")
+    User getUser(String username);
 }
--- src/main/java/com/example/simplebbs/user/UserService.java
+++ src/main/java/com/example/simplebbs/user/UserService.java
@@ -1,11 +1,14 @@
 package com.example.simplebbs.user;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
@@ -19,5 +22,14 @@ public class UserService {
         user.setUsername(username);
         user.setPassword(passwordEncoder.encode(password));
         userMapper.insertUser(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userMapper.getUser(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return user;
     }
 }
