fe04e97c63121b531fc4a28c6c2843e4ae0bdeec
dsyang <dsyang.tech@gmail.com>
2024-07-15 11:42:58 +0900

    회원 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO `article` (`subject`, `contents`, `author`) VALUES
 ('첫 번째 글', '첫 번째 글의 내용입니다.', '홍길동'),
 ('두 번째 글', '두 번째 글의 내용입니다.', '이몽룡');
+
+INSERT INTO `user` (`username`, `password`)
+VALUES
+	('user', '{bcrypt}$2a$10$TS7MzAWpYfTSztMgpJ.cl.l1hy5TFbuY.HDMB4oRnRqYwaSq9jZ/2');
