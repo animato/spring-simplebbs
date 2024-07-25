811c099ebfb738668194fc0ae646bb9593aee07b
dsyang <neoanimato@gmail.com>
2024-07-25 20:10:11 +0900

    Mapper 설명 추가

--- src/main/java/com/example/simplebbs/article/ArticleMapper.java
+++ src/main/java/com/example/simplebbs/article/ArticleMapper.java
@@ -4,7 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper
+@Mapper // 쿼리를 XML에 적용하더라도 @Mapper 어노테이션이 없으면 안됨
 public interface ArticleMapper {
     void insertArticle(Article article);
 
